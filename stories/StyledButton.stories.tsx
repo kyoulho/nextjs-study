65e4d9fc59ca73d2c8262aefab55b64ded4140e4
jinhyun <kyoulho@gmail.com>
2024-05-13 22:36:22 +0900

    24.05.13

--- stories/StyledButton.stories.tsx
+++ stories/StyledButton.stories.tsx
@@ -1,18 +1,31 @@
-// 파일 안의 스토리 설정(메타데이터 객체)
 import {StyledButton} from "@/components/StyledButton";
 import {action} from "@storybook/addon-actions";
 import React, {useState} from "react";
 
+// 파일 안의 스토리 설정(메타데이터 객체)
 export default {
     // 그룹명
     title: 'StyledButton',
+    // 자동으로 문서 생성
+    tags: ["autodocs"],
     // 사용하는 컴포넌트
     component: StyledButton,
+    // 매개변수를 스토리북에서 변경할 수 있도록 추가
+    argTypes: {
+        variant: {
+            control: {type: "radio"},
+            options: ["primary", "success", "transparent"],
+        },
+        children: {
+            control: {type: "text"}
+        }
+    }
 };
 
 // increment 라는 이름으로 action을 출력하기 위한 함수를 만든다.
 const incrementAction = action('increment');
 
+// 함수 컴포넌트로 선언하는 방식
 export const Primary = (props) => {
     const [count, setCount] = useState(0);
     const onClick = (e: React.MouseEvent) => {
@@ -28,17 +41,18 @@ export const Primary = (props) => {
     );
 };
 
-export const Success = (props) => {
-    return (
-        <StyledButton {...props} variant={"success"}>
-            Primary
-        </StyledButton>
-    );
+// 객체 리터럴로 선언하는 방식
+export const Success = {
+    args: {
+        variant: "success",
+        children: "Success",
+    }
 };
-export const Transparent = (props) => {
-    return (
-        <StyledButton {...props} variant={"transparent"}>
-            Primary
-        </StyledButton>
-    );
+
+export const Transparent = {
+    args: {
+        variant: "transparent",
+        children: "Transparent",
+    }
 };
+
